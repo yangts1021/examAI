2cbdda231afb82e13943bb8c786ca82a5fdfc28c
Rick <rick.th.yang@gmail.com>
2026-01-12 22:55:46 +0800

    update: change constants

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 // 在這裡貼上您的 Google Apps Script 網頁應用程式網址
 // 請務必將網址貼在引號內，例如: "https://script.google.com/macros/s/AKfycbx.../exec"
-export const GOOGLE_APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyPnWaTsYWV2IBiVMtmgevrMyx87DulDsp3bEhx_1E_YLHPp4FBAvCrPlkImUuBy8R6/exec'; 
+export const GOOGLE_APP_SCRIPT_URL = '';
 
 export const GEMINI_API_KEY = process.env.API_KEY || '';
